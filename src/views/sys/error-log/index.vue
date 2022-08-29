9062cdf049fe3225f5eee449b8239f29b59fefa5
chuck <chuck.qukun@gmail.com>
2022-08-29 19:25:28 +0800

    docs: remove imported module

--- src/views/sys/error-log/index.vue
+++ src/views/sys/error-log/index.vue
@@ -12,9 +12,6 @@
         <a-button @click="fireResourceError" type="primary">
           {{ t('sys.errorLog.fireResourceError') }}
         </a-button>
-        <a-button @click="fireAjaxError" type="primary">
-          {{ t('sys.errorLog.fireAjaxError') }}
-        </a-button>
       </template>
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
@@ -41,7 +38,6 @@
   import { useMessage } from '/@/hooks/web/useMessage';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { useErrorLogStore } from '/@/store/modules/errorLog';
-  import { fireErrorApi } from '/@/api/demo/error';
   import { getColumns } from './data';
   import { cloneDeep } from 'lodash-es';
 
@@ -89,9 +85,5 @@
 
   function fireResourceError() {
     imgList.value.push(`${new Date().getTime()}.png`);
-  }
-
-  async function fireAjaxError() {
-    await fireErrorApi();
   }
 </script>
